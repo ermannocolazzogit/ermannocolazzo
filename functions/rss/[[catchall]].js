7f40c8c62403a12be75bc6da7c543720b2517aff
ermannocolazzogit <ermanno.colazzo@gmail.com>
2026-09-14 13:35:58 +0200

    Aggiunge route 410 per RSS legacy

---
+++ functions/rss/[[catchall]].js
@@ -0,0 +1,10 @@
+export function onRequest() {
+  return new Response("Gone", {
+    status: 410,
+    headers: {
+      "Cache-Control": "public, max-age=86400",
+      "Content-Type": "text/plain; charset=utf-8",
+      "X-Robots-Tag": "noindex",
+    },
+  });
+}
